8b58746c4b9a38ef1d0c5b81804d59d1417730f1
TheMafiaBot <98544894+TheMafiaBot@users.noreply.github.com>
2022-06-30 09:34:09 +0530

    Add files via upload

--- spambot/helpers/callbackQuery.py
+++ spambot/helpers/callbackQuery.py
@@ -2,7 +2,7 @@ from spambot import *
 from spambot import MafiaBot1, MafiaBot2, MafiaBot3, MafiaBot4, MafiaBot5
 from spambot.help import *
 from spambot.helpers.commands import *
-from telethon.sync import events
+from telethon import events
 
 
 @MafiaBot1.on(events.CallbackQuery(data=b'alive'))
